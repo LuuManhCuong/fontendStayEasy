1b071333e243d47fdd4fd8a80658bc90259c61a9
hoainam-dev <nam54529@donga.edu.vn>
2024-02-20 10:55:05 +0700

    namhh-airbnb-configtaiwind_commit code config tailwind css and UI Login

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      backdropBlur: {
+        xs: '2px',
+        sm: '4px',
+        DEFAULT: '5px',
+        lg: '8px',
+        xl: '16px'
+      }
+    },
+  },
+  plugins: [],
+}
+
