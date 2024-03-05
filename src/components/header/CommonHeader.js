b7c89bc2351ddc546afddc6be4e4464d7b8a4b2f
hoainam-dev <nam54529@donga.edu.vn>
2024-03-05 08:43:23 +0700

    commit

--- src/components/header/CommonHeader.js
+++ src/components/header/CommonHeader.js
@@ -9,9 +9,7 @@ export default function CommonHeader({padding}) {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 
   // check is loginned yet?
-  const [isLogined, setIsLogined] = useState(
-    localStorage.getItem("user") ? true : false
-  );
+  const [isLogined, setIsLogined] = useState(localStorage.getItem("user") ? true : false);
   
   return (
     <>
@@ -29,7 +27,7 @@ export default function CommonHeader({padding}) {
             <NavLink to="/host/home" className={(navData) => navData.isActive ? "active hover:bg-gray-100 p-3 rounded-2xl text-[1.5rem] mt-1" : "hover:bg-gray-100 p-3 rounded-full text-[1.5rem] mt-1"}>
               Cho thuê chỗ ở qua Stayeasy
             </NavLink>
-            <button className="hover:bg-gray-100 p-3 rounded-[100%]" onClick="">
+            <button className="hover:bg-gray-100 p-4 rounded-[100%]" onClick="">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" aria-hidden="true" role="presentation" focusable="false" className="block h-7 w-7"><path d="M8 .25a7.77 7.77 0 0 1 7.75 7.78 7.75 7.75 0 0 1-7.52 7.72h-.25A7.75 7.75 0 0 1 .25 8.24v-.25A7.75 7.75 0 0 1 8 .25zm1.95 8.5h-3.9c.15 2.9 1.17 5.34 1.88 5.5H8c.68 0 1.72-2.37 1.93-5.23zm4.26 0h-2.76c-.09 1.96-.53 3.78-1.18 5.08A6.26 6.26 0 0 0 14.17 9zm-9.67 0H1.8a6.26 6.26 0 0 0 3.94 5.08 12.59 12.59 0 0 1-1.16-4.7l-.03-.38zm1.2-6.58-.12.05a6.26 6.26 0 0 0-3.83 5.03h2.75c.09-1.83.48-3.54 1.06-4.81zm2.25-.42c-.7 0-1.78 2.51-1.94 5.5h3.9c-.15-2.9-1.18-5.34-1.89-5.5h-.07zm2.28.43.03.05a12.95 12.95 0 0 1 1.15 5.02h2.75a6.28 6.28 0 0 0-3.93-5.07z"></path></svg>
             </button>
             {/* Menu */}
@@ -52,7 +50,7 @@ export default function CommonHeader({padding}) {
                   </div>
                 </DropdownToggle>
                 {isLogined ? (
-                  <Authenticated setIsLogined={setIsLogined}></Authenticated>
+                  <Authenticated setIsLogined={setIsLogined} />
                 ) : (
                   <AuthModal setIsLogined={setIsLogined} />
                 )}
@@ -60,7 +58,7 @@ export default function CommonHeader({padding}) {
             </div>
           </div>
           {isLogined?
-          <div className="min-[768px]:hidden max-[768px]:block font-medium text-3xl">Welcome back, {user.lastName}</div>
+          <div className="min-[768px]:hidden max-[768px]:block font-medium text-3xl">Welcome back, {user?.lastName}</div>
           :
           <div className="min-[768px]:hidden max-[768px]:block font-medium text-3xl">Welcome to Stayeasy</div>
           }
